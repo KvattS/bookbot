1903f818ace3fd37ef3113f8425225bc2aa50cfc
KvattS <kristoffer.skantz@outlook.com>
2024-01-13 12:25:27 +0100

    The main.py program now reads the book in books/

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello GitHub")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
