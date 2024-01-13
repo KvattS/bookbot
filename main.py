afb79d173eb4c677fcf4fcf21cd60ba7e3bc6328
KvattS <kristoffer.skantz@outlook.com>
2024-01-13 13:04:13 +0100

    counted the words in the book in books/

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
 
 def get_book_text(path):
